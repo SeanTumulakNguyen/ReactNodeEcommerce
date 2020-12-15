eabe93e55bb9e06f5d65cfd4de036b7ad75a5f9f
Sean Nguyen <sean.nguyenf@yahoo.com>
2020-12-14 23:34:14 -0500

    connect mongoose, mongo atlas

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose')
+
 require('dotenv').config();
 
+// app
+const app = express();
+
+// database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Database is connected')
+}).catch((err) => {
+    console.log(err)
+})
+
+// routes
 app.get('/', (req, res) => {
 	res.send('hello from node');
 });
 
+// port
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
